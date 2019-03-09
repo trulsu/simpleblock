d2d2d822d1b925090e86bef4b4e5cfbc93713117
Truls Unstad <truls.unstad@gmail.com>
2019-03-09 10:04:17 +0100

    Adding a custom block and two recipes.

--- src/main/java/net/nittini/simpleblock/init/ModBlocks.java
+++ src/main/java/net/nittini/simpleblock/init/ModBlocks.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.nittini.simpleblock.blocks.CoolBlock;
 import net.nittini.simpleblock.blocks.RubyBlock;
 
 public class ModBlocks {
     public static final List<Block> BLOCKS = new ArrayList<Block>();
 
     public static final Block RUBY_BLOCK = new RubyBlock("ruby_block", Material.IRON);
+    public static final Block COOL_BLOCK = new CoolBlock("cool_block", Material.IRON);
 }
